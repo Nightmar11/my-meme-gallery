838a17dc9ce244ff28cbb98393909a37436c9ec7
Your Name <you@example.com>
2026-01-17 18:32:35 +0800

    添加Supabase集成

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://ondubkofktxgfzwgihwp.supabase.co" 
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZHVia29ma3R4Z2Z6d2dpaHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0Mzc5NjMsImV4cCI6MjA4MzAxMzk2M30.ab2w-vpTbFaFbSGwX7uzWFxfKPRTDv_Um3U3H8QFCGM"
 export const supabase = createClient(supabaseUrl, supabaseKey)
